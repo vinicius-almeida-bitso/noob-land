0aa224edec767b43a88bc22dc12f16bdf6b0fe52
Vinicius Almeida <vinicius.almeida@bitso.com>
2024-05-06 22:31:05 -0300

    noob commit

---
+++ virtual-threads/src/main/java/com/bitso/PlatformThreads.java
@@ -0,0 +1,43 @@
+package com.bitso;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class PlatformThreads {
+
+    static class Transfer {
+        public int amount;
+        public Transfer(int amount) {
+            this.amount = amount;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        var timestamp = Instant.now().toEpochMilli();
+//        var atomicInt = new AtomicInteger(0);
+    
+    //        try (var executor = Executors.newFixedThreadPool(8)){
+    //            IntStream.range(0, 50_000_000).forEach(i ->
+    //                executor.submit(atomicInt::getAndIncrement));
+    //        }
+        
+        var atomicInt = new AtomicInteger(0);
+        
+        try (var executor = Executors.newFixedThreadPool(8)) {
+            IntStream.range(0, 10_000).forEach(i ->
+                executor.submit(() -> {
+                    Thread.sleep(Duration.ofMillis(20));
+                    System.out.printf("Thread name=%s | Thread id=%s\n", Thread.currentThread().getName(), Thread.currentThread().threadId());
+                    return atomicInt.getAndIncrement();
+                }));
+        }
+        
+        System.out.printf("time elapsed=%s%n", Instant.now().toEpochMilli() - timestamp);
+        System.out.println(atomicInt.get());
+    }
+
+}
---
+++ virtual-threads/src/main/java/com/bitso/VirtualThreads.java
@@ -0,0 +1,42 @@
+package com.bitso;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class VirtualThreads {
+
+    static class Transfer {
+        public int amount;
+        public Transfer(int amount) {
+            this.amount = amount;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        var timestamp = Instant.now().toEpochMilli();
+        var atomicInt = new AtomicInteger(0);
+    
+    //        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+    //            IntStream.range(0, 50_000_000).forEach(i ->
+    //                executor.submit(atomicInt::getAndIncrement));
+    //        }
+        
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            IntStream.range(0, 10_000).forEach(i ->
+                executor.submit(() -> {
+                    Thread.sleep(Duration.ofMillis(20));
+                    System.out.printf("Thread name=%s | Thread id=%s\n", Thread.currentThread().getName(), Thread.currentThread().threadId());
+                    return atomicInt.getAndIncrement();
+                }));
+        }
+        
+        System.out.printf("time elapsed=%s%n", Instant.now().toEpochMilli() - timestamp);
+        System.out.println(atomicInt.get());
+
+    }
+
+}
